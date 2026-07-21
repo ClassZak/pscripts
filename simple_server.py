9e69946fb895dbcac6fc820afd8f7b6c4362c39b
ClassZak <pahargolub2@gmail.com>
2026-07-21 22:10:46 +0300

    Add simple server

---
+++ simple_server.py
@@ -0,0 +1,12 @@
+#!/bin/python3
+
+import http.server
+import socketserver
+
+
+PORT = 8000
+Handler = http.server.SimpleHTTPRequestHandler
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print(f"Serving files at http://localhost:{PORT}")
+    httpd.serve_forever()
